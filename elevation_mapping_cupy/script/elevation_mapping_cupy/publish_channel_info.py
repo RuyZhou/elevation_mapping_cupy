516921e7d166abf6526fbfed8f5b199700d10b43
RuyZhou <ruyzhou@leggedrobotics.com>
2024-05-18 21:15:01 +0800

    since zed input image is 4 channel (rgba8), tricky to use a rgb_color_filter plugin to deal with the color fusion

---
+++ elevation_mapping_cupy/script/elevation_mapping_cupy/publish_channel_info.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import rospy
+from elevation_map_msgs.msg import ChannelInfo
+
+
+class ChannelInfoPub():
+    def __init__(self):
+        self.timer = rospy.Timer(rospy.Duration(1), self.publish_channel_info)
+        self.channel_info_pub = rospy.Publisher('/zed2i/zed_node/rgb/channel_info', ChannelInfo, queue_size=1)
+        
+        channel_info = ChannelInfo()
+        channel_info.channels = ['r', 'g', 'b', 'a']
+        self.channel_info = channel_info
+
+    def publish_channel_info(self, event):
+        self.channel_info.header.stamp = rospy.Time.now()
+        self.channel_info_pub.publish(self.channel_info)
+
+if __name__ == '__main__':
+    rospy.init_node('channel_info_pub')
+    channel_info_pub = ChannelInfoPub()
+
+    while not rospy.is_shutdown():
+        try:
+            rospy.spin()
+        except rospy.ROSInterruptException:
+            print("Error")
